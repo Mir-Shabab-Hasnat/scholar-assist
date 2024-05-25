773d6b8ecb1daa768a5f4a83afd2bb2b4d78bd48
Mir Shabab Hasnat <shababhasnat@gmail.com>
2024-05-24 19:26:04 -0600

    fixed server side error, added client side to authcallback page and added providers to layout

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
 
 const Page = () => {
   const router = useRouter();
@@ -7,7 +10,7 @@ const Page = () => {
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+  trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
       if (success) {
         // user is synced to db
@@ -22,6 +25,16 @@ const Page = () => {
     retry: true,
     retryDelay: 500,
   });
+
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You will be redirected automatically</p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
