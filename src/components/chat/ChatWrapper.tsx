db4c42cda4b03990a81a0b946249bc711670ae9d
Mir Shabab Hasnat <shababhasnat@gmail.com>
2024-06-09 20:37:49 -0600

    loading state created

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -3,13 +3,14 @@
 import { trpc } from "@/app/_trpc/client";
 import ChatInput from "./ChatInput";
 import Messages from "./Messages";
+import { Loader2 } from "lucide-react";
 
 interface ChatWrapperProps {
   fileId: string;
 }
 
 const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
-  const {} = trpc.getFileUploadStatus.useQuery(
+  const { data, isLoading } = trpc.getFileUploadStatus.useQuery(
     {
       fileId,
     },
@@ -18,6 +19,21 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
         data?.status === "SUCCESS" || data?.status === "FAILED" ? false : 500,
     }
   );
+
+  if (true)
+    return (
+      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className="flex-1 flex justify-center items-center flex-col mb-28">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
+            <h3 className="font-semibold text-xl">Loading...</h3>
+            <p className="text-zinc-500 text-sm">
+              We&apos;re preparing your PDF
+            </p>
+          </div>
+        </div>
+      </div>
+    );
 
   return (
     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
